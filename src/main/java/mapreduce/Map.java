bfb7626e1a0c5f03718aa5079bf13c967ecbbd16
Clément Lavoisier <clem.lavoisier@hotmail.fr>
2016-10-10 10:29:26 +0200

    classe map reduce driver

--- src/main/java/mapreduce/Map.java
+++ src/main/java/mapreduce/Map.java
@@ -1,5 +1,41 @@
 package mapreduce;
 
-public class Map {
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class Map extends Mapper<LongWritable, Text, Text, Text> {
+
+    private Text vertexID = new Text();
+    private Text vertice=new Text();
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String line = value.toString();
+        StringTokenizer itr = new StringTokenizer(line,"\n");
+        StringTokenizer itrInside;
+
+
+
+        while (itr.hasMoreTokens()) {
+            if(itr.countTokens() > 2){
+
+            }//ignore first line ??
+            else{
+                itrInside=new StringTokenizer(itr.toString());
+                vertexID.set(itr.nextToken());
+
+                while(itrInside.hasMoreTokens()){               
+                    vertice.set(itrInside.nextToken());
+                    context.write(vertexID, value);
+                }           
+            }
+        }
+
+    }
 
 }
+
--- src/main/java/mapreduce/Reduce.java
+++ src/main/java/mapreduce/Reduce.java
@@ -1,5 +1,28 @@
 package mapreduce;
 
-public class Reduce {
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class Reduce{
+    @SuppressWarnings("unchecked")
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+        String vertices="";
+
+        for (Text value : values) {
+            if(vertices=="")
+                vertices+=value.toString();
+            else
+                vertices=vertices+","+value.toString();         
+        }
+
+        Text value=new Text();
+        value.set(vertices);
+        context.write(key, value);
+
+    }
 
 }
+
